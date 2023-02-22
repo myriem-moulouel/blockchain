e6ef87ab45f48dd797e8ab2fa3a785d427ab3638
myriem-moulouel <60098131+myriem-moulouel@users.noreply.github.com>
2023-02-22 11:48:57 +0100

    Add files via upload

--- node.py
+++ node.py
@@ -31,18 +31,17 @@ class Node:
     Possède à la fois le code du client et du serveur (fait les deux).
     """
 
-    known_peers: list[str] = []
-
     def __init__(self):
         # Choose a random port to open the server on.
         self.address = "localhost"
         self.port = int(sys.argv[1])
 
-        self.list_adresses = []
-        self.list_connections = set()
+        self.list_connections = []
+        self.connx = []
 
         print("On est sur l'adresse et le port suivant", self.address, self.port)
         self._listen_thread = th.Thread(target=self._listen)
+        self._broadcast_thread = th.Thread(target=self.broadcast)
         if len(sys.argv)>2:
             self._connect(int(sys.argv[2]))
         else:
@@ -52,9 +51,9 @@ class Node:
         self._stop_event = th.Event()
         
 
-
     def run_thread(self):
         self._listen_thread.start()
+        self._broadcast_thread.start()
 
 
     def _send_msg(self, socket, msg):
@@ -71,17 +70,16 @@ class Node:
             server_socket.bind((self.address, self.port))
             server_socket.listen()
             while not self._stop_event.is_set():
+
                 connection, address = server_socket.accept()
                 print(f"connection from{address} has been established")
-                self.list_adresses.append(address)
-                self.list_connections.add(connection)
+                
                 #send a message
                 self._send_msg(connection, f"You're connected to {self.address, self.port}")
-                
+
                 #receive a massage
                 print(self._receive_msg(connection))
 
-            for connection in self.list_connections:
                 connection.close()
 
             print("connection is closed")
@@ -92,13 +90,29 @@ class Node:
             address = "localhost"
             client_socket.connect((address, port))
 
-            self.list_adresses.append((address, port))
-            self.list_connections.add(client_socket)
+            self.list_connections.append(port)
+
             #receive a message
             print(self._receive_msg(client_socket))
+
             #send a message
             self._send_msg(client_socket,f"thank you for accepting the connection from {self.address, self.port}!")
         
 
+
+    def broadcast(self):
+        for i in range(len(self.list_connections)):
+            
+            port = int(self.list_connections[i])
+
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+                address = "localhost"
+                client_socket.connect((address, port))
+                
+                #send a message
+                self._send_msg(client_socket,f"Here my list of connections { self.list_connections }")
+
+    def minage(self):
+        pass
 
 n = Node()
